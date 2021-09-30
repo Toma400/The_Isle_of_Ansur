e50a4d7b16fcde4debdcaccb70d854812532da7f
Toma400 <65849429+Toma400@users.noreply.github.com>
2021-09-30 20:22:44 +0000

    Bugfix related to starting the game

--- gui/character.py
+++ gui/character.py
@@ -5,11 +5,19 @@ def name():
   print (utils.text.text_align(utils.colours.bcolors.OKBLUE + "--------------------" + utils.colours.bcolors.ENDC, "centre_colour"))
   print ("\n")
   print (utils.text.text_align("Type your character name", "centre"))
+  print (utils.text.text_align("---", "centre"))
+  print (utils.text.text_align("Type -0- to cancel", "centre"))
+  print ("\n")
   while True:
     player_name = input ("")
     if system.save_system.folder_creating(player_name) == False:
-      pass
-    else:
+      name()
       break
-      #theoretically, it should be another step in character creation here
-      #but "break" can be useful just in case loop will continue
+    else:
+      #actually that part should already export 'player_name' to 'in_use' profile.json
+      #and gender should have no arguments, since no need is necessary
+      gender (player_name)
+      break
+
+def gender(name):
+  print (name)
