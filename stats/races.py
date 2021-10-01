6f59bbffbd698b724caf5f3938dbbf33d8ecc6aa
Toma400 <65849429+Toma400@users.noreply.github.com>
2021-10-01 12:15:28 +0000

    Adding races (nord, lunanian, lunatri) + race_id

--- stats/races.py
+++ stats/races.py
@@ -3,6 +3,7 @@
 #spec_moon - pwr_void X pwr_conn penalty not affecting
 
 class race_baedoorian:
+  race_id = "ansur:baedoorian"
   descript = "Human, Born in Baedoor"
   atr_strength = 2
   atr_charisma = 1
@@ -15,17 +16,42 @@ class race_baedoorian:
   spec_moon = False
 
 class race_westernwaldian:
+  race_id = "ansur:westernwaldian"
   descript = "Human, Born in WesternWald"
   atr_strength = 1
   atr_charisma = 1
   atr_intelligence = 1
-  pwr_magic = 1
+  pwr_magic = 2
   abil_trade = 1
   abil_longswords = 1
   spec_diamond = False
   spec_moon = False
 
+class race_nord:
+  race_id = "ansur:nord"
+  descript = "Human, Born in Northern Isles"
+  atr_strength = 2
+  atr_endurance = 1
+  atr_charisma = 1
+  atr_intelligence = -1
+  abil_shortswords = 1
+  abil_toughness = 1
+  spec_diamond = False
+  spec_moon = False
+
+class race_lunanian:
+  race_id = "ansur:lunanian"
+  descript = "Human, Born in Center of Moon Continent"
+  atr_strength = 1
+  atr_endurance = 1
+  atr_charisma = 1
+  abil_trade = 1
+  abil_resource_processing = 1
+  spec_diamond = False
+  spec_moon = False
+
 class race_saphtri:
+  race_id = "ansur:saphtri"
   descript = "Saphtri"
   atr_strength = -1
   atr_dexterity = 2
@@ -37,6 +63,7 @@ class race_saphtri:
   spec_moon = False
 
 class race_voitri:
+  race_id = "ansur:voitri"
   descript = "Voitri"
   atr_charisma = 1
   atr_dexterity = 1
@@ -49,7 +76,19 @@ class race_voitri:
   spec_diamond = False
   spec_moon = False
 
+class race_lunatri:
+  race_id = "ansur:lunatri"
+  descript = "Lunatri"
+  atr_charisma = 1
+  atr_dexterity = 1
+  atr_intelligence = 1
+  pwr_magic = 5
+  abil_transformation_magic = 1
+  abil_persuasion = 1
+  abil_alchemy = 1
+
 class race_ormath:
+  race_id = "ansur:ormath"
   descript = "Ormath"
   atr_strength = -1
   atr_intelligence = 2
@@ -60,10 +99,12 @@ class race_ormath:
   pwr_void = -20
   abil_connection = 1
   abil_healing = 1
+  abil_survival = 1
   spec_diamond = False
   spec_moon = False
 
 class race_ett:
+  race_id = "ansur:ett"
   descript = "Ett"
   atr_strength = 1
   atr_dexterity = -1
@@ -76,6 +117,7 @@ class race_ett:
   spec_moon = False
 
 class race_ghlodd:
+  race_id = "ansur:ghlodd"
   descript = "Ghlodd"
   atr_strength = 1
   atr_endurance = 1
