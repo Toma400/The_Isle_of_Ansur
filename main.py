7f1f7f741493981867863f35b37a233f344a1708
Toma400 <65849429+Toma400@users.noreply.github.com>
2021-10-03 22:55:05 +0000

    BIG VICTORY. .JSON managment functions done ヽ( •_)ᕗ

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ import gui.menu
 #import system.id_manag
 #system.id_manag.rid_conv ("Ansur:", "descript")
 
-gui.menu.start()
+#gui.menu.start()
 #use "test" answer to use latest working feature (.json manag)
+
+import system.json_manag
+print (system.json_manag.save_read("Sandra", "profile", "hp_level"))
+system.json_manag.save_change("Sandra", "profile", "hp_level", "math", -10)
--- system/json_manag.py
+++ system/json_manag.py
@@ -14,14 +14,48 @@ def json_change(path, element, change_type, change_value):
   #^ function a bit obsolete, but can be used to non-save related things, such as objects with non-changeable directory (default worlds/Ansur elements, for example)
   #for [modded] worlds/X elements though, another loader can be necessary
 
-def save_read(name, category, element):
+def save_read(name, category, element, dict_type=False):
   import json
-  final_path = "saves/" + name + "/" + category + ".json"
+  final_path = "saves/" + name + "/in_use/" + category + ".json"
+  data = {}
+  with open(final_path) as json_file:
+    data = json.load(json_file)
+  if dict_type == False:
+    return data[element]
+  else:
+    return data
   #element tells what type of variable is needed to be read
 
 def save_change(name, category, element, change_type, change_value):
-  #same as json_change, but with save_read principles
-  #-> since json_change couldn't read /relative/ path for player, since it has no name argument (therefore you should create final_path manually everytime)
-  save_read(name, category, element)
-  #operate on save_read
-  pass
+  import json
+  final_path = "saves/" + name + "/in_use/" + category + ".json"
+  #simply replacing value with new one (usually for string variables)
+  if change_type == "replacement":
+    temp_dict = save_read(name, category, element, True)
+    temp_dict[element] = change_value
+    with open (final_path,'w') as file:
+      json.dump(temp_dict, file, indent = 2)
+  #math is intended to change value with +/- (use negative value to make it smaller)
+  elif change_type == "math":
+    temp_dict = save_read(name, category, element, True)
+    temp_dict[element] = temp_dict[element] + change_value
+    with open (final_path,'w') as file:
+      json.dump(temp_dict, file, indent = 2)
+  #math, but for (*) [rare case]
+  elif change_type == "math*":
+    temp_dict = save_read(name, category, element)
+    temp_dict[element] = temp_dict[element] * change_value
+    with open (final_path,'w') as file:
+      json.dump(temp_dict, file, indent = 2)
+  #math, but for (/) [rare case]
+  elif change_type == "math/":
+    temp_dict = save_read(name, category, element)
+    temp_dict[element] = temp_dict[element] / change_value
+    with open (final_path,'w') as file:
+      json.dump(temp_dict, file, indent = 2)
+  #var_add is intended to be dict; can be useful with version_updater especially
+  elif change_type == "var_add":
+    temp_dict = save_read(name, category, element, True)
+    temp_dict.update (change_value)
+    with open (final_path,'w') as file:
+      json.dump(temp_dict, file, indent = 2)
