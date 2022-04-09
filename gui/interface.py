560a4a3a277507d4611ceb09d3e10b7b89516e8c
Toma400 <thomasso_calgori@o2.pl>
2022-04-09 18:45:00 +0200

    Change to root to redirect player to menu (for testing purposes, makes game only able to create new profile)

--- gui/interface.py
+++ gui/interface.py
@@ -1,5 +1,9 @@
 def main_game (name):
-  print ("Oh!")
-  while True:
+  #print ("Oh!")
+  #while True:
       # main content of the game, with some function redirections which should be loops to avoid recursions
-      break
+  #    break
+
+  # temporary element - just to make first version of the game for testing
+  import menu
+  menu.start()
