933ffe7e21de3337b73ed5f82a2a32bb0b6fb4fd
Toma400 <thomasso_calgori@o2.pl>
2022-04-13 00:14:15 +0200

    Exception handler for whole game introduced (part of #15 implementation)

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import system.cache.cache_manag
 system.cache.cache_manag.cache_redirect()
+from utils.text_manag import colour_formatter as format
 import gui.menu
+import traceback
 
-gui.menu.start()
+try:
+    gui.menu.start()
+except:
+    print("---------------------------------------------------------")
+    traceback.print_exc()
+    print("\n")
+    print("---------------------------------------------------------")
+    print(format("red", "Found an error! See the message above for details"))
+    print(format("yellow", "You can send message above to developer, reporting the issue"))
+    print("\n")
+    print(format("yellow", "Enter any key to close the game"))
+    temp_var = input("")
