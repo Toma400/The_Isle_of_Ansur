650f464a4a0bc1270db6cf8a3d27607b9478c1f1
Toma400 <thomasso_calgori@o2.pl>
2022-10-25 16:15:40 +0200

    Created BCS Build file

---
+++ bcs.py
@@ -0,0 +1,10 @@
+# BCS SHOULD BE GENERATOR
+# So project folder should not have structure for mod, but rather instructions
+# Folder structure should appear only when you click on "export" button, and should be in _bcs folder (sort of _temp variant)
+
+while True:
+    print ("----------------- Baedoor Creation Set -----------------")
+    a = input ("[                 Input -exit- to exit                 ]\n")
+
+    if a == "exit":
+        break
