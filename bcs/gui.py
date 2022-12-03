7165a8cf2c1143ebdbc6b0d4cafb9dea79808d0f
Toma400 <thomasso_calgori@o2.pl>
2022-12-03 19:28:06 +0100

    Some further investments

--- bcs/gui.py
+++ bcs/gui.py
@@ -2,7 +2,7 @@ from bcs.objects import Screen, GUI_Helper
 from bcs.operators import langstr, cells
 import pygame, pygame_gui
 
-def event_runner(con, pg_events, bcs_events, dyn_screen):
+def event_runner(con, guitype, pg_events, bcs_events, dyn_screen):
     guiobj = dyn_screen.gui        # all objects used later to be drawn
     pg_gui = dyn_screen.gui_manag  # pygame_gui manager
 
@@ -11,11 +11,12 @@ def event_runner(con, pg_events, bcs_events, dyn_screen):
             con.append("end")
 
         if event.type == pygame_gui.UI_SELECTION_LIST_NEW_SELECTION:
-            bcs_events["sl_in"] = event.ui_element
+            bcs_events["sl_lbox"] = event.ui_element
 
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == guiobj.login__enter and bcs_events["sl_in"].get_single_selection() is not None:
-                print(bcs_events["sl_in"].get_single_selection())
+            if event.ui_element == guiobj.login__enter and bcs_events["sl_lbox"].get_single_selection() is not None:
+                print(bcs_events["sl_lbox"].get_single_selection())
+                guitype[0] = "logged"; dyn_screen.guitype_switch(guitype[0])
 
         pg_gui.process_events(event)
 
--- bcset.py
+++ bcset.py
@@ -4,13 +4,13 @@ from bcs.gui import gui_runner, event_runner
 import pygame_gui
 
 def run():
-    dyn_screen = Screen()             # pygame screen
     guitype    = ["login", None]      # gui category being currently used
+    dyn_screen = Screen(guitype[0])   # pygame screen
     guiobj     = dyn_screen.gui       # all objects used later to be drawn
     pg_gui     = dyn_screen.gui_manag # pygame_gui manager
 
     bcs_events = {
-        "sl_in": None
+        "sl_lbox": None     # selected listbox
     }
 
     clock = pygame.time.Clock()
@@ -22,7 +22,7 @@ def run():
         pg_events = pygame.event.get()  # variablised so it can be passed to functions w/o calling more than one per frame
 
         gui_runner(screen, con, guitype, guiobj, pg_events, dyn_screen) # manager for everything on screen
-        event_runner(con, pg_events, bcs_events, dyn_screen)
+        event_runner(con, guitype, pg_events, bcs_events, dyn_screen)
 
         pg_gui.update(delta)
         pg_gui.draw_ui(screen)
