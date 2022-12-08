56b431f7f94fd6cf20e1c62b13ae6e8e46a7b107
Toma400 <thomasso_calgori@o2.pl>
2022-12-08 13:24:24 +0100

    Updated & .json-driven system_ref (for further requests module, most possibly)

--- core/system_ref.py
+++ core/system_ref.py
@@ -6,11 +6,14 @@ import os; gpath = os.path.dirname(os.path.abspath("main.py"))
 
 class SysRef:
 
-    name = "The Isle of Ansur"
-    status = "pre alpha"
+    name    = "The Isle of Ansur"
+    status  = "pre alpha"
     version = "3"
 
-    save_system = "1.0"
+    cname   = "Between Shadows and Light"
+    cycle   = "0"
+
+    save_system     = "1.0"
     vanilla_modules = [
         "ansur"
     ]
