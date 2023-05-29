09139d013cb3c6cf344d6e8eed84448d1c89e7df
Toma400 <thomasso_calgori@o2.pl>
2023-05-29 21:01:30 +0200

    Fixed `get_mods*` function by removing tail of returned paths

--- bcs/operators.nim
+++ bcs/operators.nim
@@ -36,9 +36,15 @@ proc langstr* (key: string): string =
   return tomlread(bcsd() & "/bcs/lang/" & lang & ".toml", key)
 
 proc get_mods* (pt: PT): seq[string] =
+  proc rm_tail (sq: seq[string], tail: string): seq[string] = # removes "tail" from every item from sequence
+    var sq_wt: seq[string]
+    for ti in sq:
+      sq_wt.add(ti.replace(tail, ""))
+    return sq_wt
+
   case pt:
-    of PT.MODS:     return toSeq(walkFiles("mods/*"))
-    of PT.PROJECTS: return toSeq(walkDirs("bcs/projects/"))
+    of PT.MODS:     return rm_tail(toSeq(walkFiles("mods/*")),        tail=r"mods\")
+    of PT.PROJECTS: return rm_tail(toSeq(walkDirs("bcs/projects/*")), tail=r"bcs\projects\")
 
 proc window_update* (window: Window) =
   window.width    = parseInt(settings("res_x"))
