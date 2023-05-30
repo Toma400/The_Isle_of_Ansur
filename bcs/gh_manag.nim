446557d037de6d823a466630ca9147683209d5ef
Toma400 <thomasso_calgori@o2.pl>
2023-05-30 12:39:46 +0200

    Change of `returnCell` functions to return int

--- bcs/gh_manag.nim
+++ bcs/gh_manag.nim
@@ -6,13 +6,13 @@ type
     X, Y
 
 # Returns pixels of specific % of screen passed
-proc returnCell* (pos: int, axis: AXES): float =
+proc returnCell* (pos: int, axis: AXES): int =
   var svc: float
   if axis == AXES.X: svc = parseInt(settings("res_x")) / 100
   else:              svc = parseInt(settings("res_y")) / 100
-  return pos.float * svc
+  return (pos.float * svc).int
 
 # Returns pixels of specific % of screen passed
-proc returnCells* (pos_x: int, pos_y: int): seq[float] =
+proc returnCells* (pos_x: int, pos_y: int): seq[int] =
     return @[returnCell(pos_x, AXES.X),
              returnCell(pos_y, AXES.Y)]
