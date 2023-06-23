bc87222f4ce6a8d172b512553ad2f02466642e5f
Toma400 <thomasso_calgori@o2.pl>
2023-06-23 16:19:27 +0200

    Fallback handling for font & logo handling

--- core/file_system/theme_manag.py
+++ core/file_system/theme_manag.py
@@ -1,11 +1,14 @@
 from core.file_system.repo_manag import gpath
 from core.utils import scx
+import logging as log
 import glob
 import toml
 #==================================================
 # GENERAL
 #==================================================
-fallback_id = toml.load(f"core/system_ref.toml")["vanilla_modules"][0]
+class Fallback:
+    id    = toml.load(f"core/system_ref.toml")["vanilla_modules"][0]
+    fsize = 1.0
 # 1. guide.toml for themes
 # 2. theme.toml from theme searched
 
@@ -18,7 +21,7 @@ fallback_id = toml.load(f"core/system_ref.toml")["vanilla_modules"][0]
 # Checks for currently used theme (theme can be changed in settings or by mod)
 def get_theme():
     tf = toml.load(f"themes/guide.toml")
-    if tf["behaviour"]["prioritised_id"] == "": return fallback_id
+    if tf["behaviour"]["prioritised_id"] == "": return Fallback.id
     else:                                       return tf["behaviour"]["prioritised_id"]
 
 def get_theme_file(id: str = get_theme()):
@@ -50,21 +53,29 @@ def font_address(theme_id: str, font_id: str):
     return f"/themes/{theme_id}/fonts/{font_id}"
 # Returns font required for current context and used language
 def font_handler(context: str, language: str = scx("lang"), overwrite: str = None):
-    if overwrite is not None:
-        return overwrite
-    #[ standard handling ]#
-    try:    return get_theme_file()["fonts"][context][language][0]
-    except: return get_theme_file()["fonts"][context]["_"][0]
+    try:
+        if overwrite is not None:
+            return overwrite
+        #[ standard handling ]#
+        try:    return get_theme_file()["fonts"][context][language][0]
+        except: return get_theme_file()["fonts"][context]["_"][0]
+    except KeyError:
+        log.error(f"Couldn't find proper key for ID: {get_theme()}, context: {context}, language: {language}. Using fallback value.")
+        return get_theme_file(Fallback.id)["fonts"][context]["_"][0]
 # Returns font size modifier (default to 1.0)
 def font_size(context: str = None):
-    if context is None: return get_theme_file()["fonts"]["general"]["font_size"]
-    else:
-        try:    return float(get_theme_file()["fonts"][context][scx("lang")][1])
-        except: return float(get_theme_file()["fonts"][context]["_"][1])
+    try:
+        if context is None: return get_theme_file()["fonts"]["general"]["font_size"]
+        else:
+            try:    return float(get_theme_file()["fonts"][context][scx("lang")][1])
+            except: return float(get_theme_file()["fonts"][context]["_"][1])
+    except KeyError:
+        log.error(f"Couldn't find font size value for ID: {get_theme()}, context: {context}. Using fallback value.")
+        return Fallback.fsize
 
 # Returns address of image that will show up in menu backgrounds
 def bg_handler(context: str, split=False):
     try:    bg_img = get_theme_file()["backgrounds"][context];            id = get_theme()
-    except: bg_img = get_theme_file(fallback_id)["backgrounds"][context]; id = fallback_id
+    except: bg_img = get_theme_file(Fallback.id)["backgrounds"][context]; id = Fallback.id
     if split is False: return f"themes/{id}/assets/{bg_img}"
     else:              return f"themes/{id}/assets/", bg_img
