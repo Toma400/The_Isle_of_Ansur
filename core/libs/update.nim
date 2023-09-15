d7bdf5815a64e35bc46a6d6164d85c7cd79d6426
Toma400 <thomasso_calgori@o2.pl>
2023-09-15 20:17:11 +0200

    Nim-Python library folder changed slightly to better work with potential Nim libs

--- core/libs/update.nim
+++ core/libs/update.nim
@@ -1,9 +1,12 @@
 # Updates all .pyd files by running them
+import std/strformat
+import std/strutils
 import std/sequtils
 import std/os
 
-const pyd_arguments = "--app:lib --out:example.pyd --threads:on --tlsEmulation:off --passL:-static"
-
 for f in toSeq(walkFiles("*.nim")):
   if f != "update.nim":
+    let pyd_name = f.replace("nim", "pyd")
+    let pyd_arguments = fmt"--app:lib --out:{pyd_name} --threads:on --tlsEmulation:off --passL:-static"
     discard execShellCmd("nim c " & pyd_arguments & " " & f)
+    echo fmt"Generated Python-DLL library: {pyd_name}"
