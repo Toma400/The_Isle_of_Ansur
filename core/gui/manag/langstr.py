169c452cb82bb5a1a403665ec9dbccff938d3e69
Toma400 <thomasso_calgori@o2.pl>
2023-10-04 19:22:35 +0200

    Fixing issue with `langstr` format to have pure try/except clauses

--- core/gui/manag/langstr.py
+++ core/gui/manag/langstr.py
@@ -8,18 +8,22 @@ import toml
 # Takes a bit different values if used with colours
 #===================================================================
 def langstring (key: str) -> str:
-    lang = scx("lang")
-    t = toml.load(f"{gpath}/core/lang/{lang}.toml")
+    try:
+        t = toml.load(f"{gpath}/core/lang/{scx('lang')}.toml")
+    except:
+        log.warning(
+            f"Internal language file does not have properly set language value for {key}.")
+        return langstring("system__text_load_fail")
     return t[key]
 
 def langjstring (key: str, modtype: str, modid: str = "ansur") -> str:
     try:
-        read = toml.load(f"{modtype}/{modid}/lang/{scx('lang')}.toml")
-    except FileNotFoundError or KeyError:
+        read = toml.load(f"{modtype}/{modid}/lang/{scx('lang')}.toml")[key]
+    except:
         try:
-            read = toml.load(f"{modtype}/{modid}/lang/english.toml")
-        except KeyError:
+            read = toml.load(f"{modtype}/{modid}/lang/english.toml")[key]
+        except:
             log.warning(
                 f"Module {modid} does not have properly set language value for {key}. Please contact the developer of this module for help.")
             return langstring("system__text_load_fail")
-    return read[key]
+    return read
