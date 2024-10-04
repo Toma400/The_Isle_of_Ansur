6351d93e95aa1b138319e1e23216e0ffd4721cda
Toma400 <thomasso_calgori@o2.pl>
2024-10-04 16:11:48 +0200

    Math haven't defeated me! (more character menu shenanigans)

--- core/gui/menus/character.py
+++ core/gui/menus/character.py
@@ -1,8 +1,11 @@
-from core.graphics.gh_manag import mouseColliderPx, mouseRec, switch_gscr, imgLoad
+from core.graphics.gh_manag import mouseColliderPx, mouseRec, switch_gscr, imgLoad, revCell
 from core.file_system.theme_manag import FontColour as fCol
 from core.graphics.text_manag import put_text
 from core.gui.manag.langstr import langstring
+from core.gui.manag.pc import toPxX, toPxY
 from os.path import exists
+
+av_spacing = revCell(toPxX(12) + toPxY(30), "x") # based on `pgui_objects` placement for `avatar` element)
 
 def characterScreen(screen, guitype, fg_events, pg_events, tev, dyn_screen):
 
@@ -10,7 +13,10 @@ def characterScreen(screen, guitype, fg_events, pg_events, tev, dyn_screen):
 
     put_text(screen, text=dyn_screen.journey.get("data.toml | name"), font_cat="menu", size=38, align_x="left", pos_x=14, pos_y=10, colour=fCol.ENABLED.value)
     # buttons
-    bex = put_text(screen, text=langstring("game__gen_back"), font_cat="menu", size=35, align_x="center", pos_y=85, colour=fCol.ENABLED.value)
+
+    agl = put_text(screen, text=langstring("char__av_gall"),  font_cat="menu", size=35, align_x="right", pos_x=av_spacing, pos_y=10, colour=fCol.DISABLED.value) # avatar gallery
+    hsw = put_text(screen, text=langstring("char__his_edit"), font_cat="menu", size=35, align_x="right", pos_x=av_spacing, pos_y=18, colour=fCol.DISABLED.value) # history edit switch
+    bex = put_text(screen, text=langstring("game__gen_back"), font_cat="menu", size=35, align_x="center",                  pos_y=85, colour=fCol.ENABLED.value)
 
     dyn_screen.put_pgui("chmn__avatar")
     dyn_screen.put_pgui("chmn__history")
